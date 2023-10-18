d57b496df979c2c3ce5b5566cbb2614ba307bccf
MrMeepsle <mynameismailtje@gmail.com>
2023-10-18 12:16:17 +0200

    Here we go agaiin

--- slurm_run_train_job.sh
+++ slurm_run_train_job.sh
@@ -15,4 +15,4 @@ export shots=5
 export queries=10
 
 srun python --version
-srun python -m src.trident_train --cnfg configs/mini-5,5/train_conf.json --dataset miniimagenet --root ./datasets/inner_set_supermarket_TRIDENT --n-ways 5 --k-shots ${shots} --q-shots ${queries} --iterations 16500 --inner-lr 0.0008 --meta-lr 0.00008 --experiment inner_classes_train_val
+srun python -m src.trident_train --cnfg configs/mini-5,5/train_conf.json --dataset custom --root ./datasets/inner_set_supermarket_TRIDENT --n-ways 5 --k-shots ${shots} --q-shots ${queries} --iterations 16500 --inner-lr 0.0008 --meta-lr 0.00008 --experiment inner_classes_train_val
