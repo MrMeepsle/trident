093f2667c1c16fd0dea12ae92a11aae1396eeb5f
MrMeepsle <mynameismailtje@gmail.com>
2023-10-22 10:31:15 +0200

    Update configs for outer train job

--- slurm_run_train_job.sh
+++ slurm_run_train_job.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
-#SBATCH --job-name="5S-10Q-trident"
+#SBATCH --job-name="5S-10Q-trident-outer"
 #SBATCH --account=education-3me-msc-ro
 #SBATCH --partition=gpu
-#SBATCH --time=11:00:00
+#SBATCH --time=23:59:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=12
 #SBATCH --gpus-per-task=1
@@ -15,4 +15,4 @@ export shots=5
 export queries=10
 
 srun python --version
-srun python -m src.trident_train --cnfg configs/mini-5,5/train_conf.json --dataset custom --root ./datasets/inner_set_supermarket_TRIDENT --n-ways 5 --k-shots ${shots} --q-shots ${queries} --iterations 16500 --inner-lr 0.0008 --meta-lr 0.00008 --experiment inner_classes_train_val
+srun python -m src.trident_train --cnfg configs/mini-5,5/train_conf.json
